3a88efee2114795b9e9c5a90618718adde7649a1
charlie2951 <90516512+charlie2951@users.noreply.github.com>
2026-05-06 21:41:32 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_krv8_ieee (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,11 +17,18 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, 1'b0};
+  wire [15:0] instr;
+  assign instr = {ui_in,uio_in};
+    krv8cpu cpu0 (
+        .clk(clk),
+        .reset(!rst_n),
+        .instruction(instr), 
+        .out_port(uo_out)      
+);
 
 endmodule
