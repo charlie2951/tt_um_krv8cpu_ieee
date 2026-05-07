3b70fa0afb0660d181dc9ea551e9e56bf82078df
charlie2951 <90516512+charlie2951@users.noreply.github.com>
2026-05-07 16:14:12 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -18,7 +18,7 @@ module tt_um_krv8_ieee (
 
   // All output pins must be assigned. If not used, assign to 0.
   assign uio_out = 0;
-  assign uio_oe  = 8'hFF;
+  assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, 1'b0};
